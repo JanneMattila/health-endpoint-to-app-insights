2011ba53e204a28d0013380c9587d79827b2de18
Janne Mattila <janne.mattila@hotmail.fi>
2022-04-30 17:43:03 +0300

    Add example

---
+++ src/Health-endpoint-to-app-insights/HealthEndpointData.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+public class HealthEndpointData
+{
+    [JsonPropertyName("status")]
+    public string Status { get; set; } = string.Empty;
+
+    [JsonPropertyName("components")]
+    public Dictionary<string, Component> Components { get; set; } = new();
+
+}
+
+public class Component
+{
+    [JsonPropertyName("status")]
+    public string Status { get; set; } = string.Empty;
+
+    [JsonPropertyName("details")]
+    public Dictionary<string, JsonElement> Details { get; set; } = new();
+}
--- src/Health-endpoint-to-app-insights/Program.cs
+++ src/Health-endpoint-to-app-insights/Program.cs
@@ -1,2 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+var builder = new ConfigurationBuilder()
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddUserSecrets<Program>()
+    .AddJsonFile("appsettings.json", optional: true)
+    .AddEnvironmentVariables();
+
+var configuration = builder.Build();
+
+var uri = configuration.GetValue<string>("uri");
+var frequency = configuration.GetValue<int>("frequency");
+var location = configuration.GetValue<string>("location");
+var connectionstring = configuration.GetValue<string>("connectionstring");
+
+Console.WriteLine($"Starting availability monitoring of {uri} every {frequency} seconds...");
+
+var client = new HttpClient();
+var telemetryClient = new TelemetryClient(new TelemetryConfiguration(connectionstring));
+
+while (true)
+{
+    var start = DateTimeOffset.UtcNow;
+    try
+    {
+        var health = await client.GetFromJsonAsync<HealthEndpointData>(uri);
+        var end = DateTimeOffset.UtcNow;
+        var metrics = new Dictionary<string, double>();
+        var properties = new Dictionary<string, string>();
+
+        if (health?.Components != null)
+        {
+            foreach (var component in health.Components)
+            {
+                properties.Add(component.Key, component.Value.Status);
+                foreach (var item in component.Value.Details)
+                {
+                    if (item.Value.ValueKind == JsonValueKind.Number)
+                    {
+                        metrics.Add($"{component.Key}_{item.Key}", item.Value.GetDouble());
+                    }
+                    else
+                    {
+                        properties.Add($"{component.Key}_{item.Key}", item.Value.GetString() ?? string.Empty);
+                    }
+                }
+            }
+        }
+
+        telemetryClient.TrackAvailability(
+            name: "health",
+            timeStamp: start,
+            duration: end - start,
+            runLocation: location,
+            success: string.Equals(health?.Status, "UP"),
+            properties: properties, metrics: metrics);
+    }
+    catch (Exception ex)
+    {
+        var end = DateTimeOffset.UtcNow;
+        telemetryClient.TrackAvailability(
+            name: "unhealthy",
+            timeStamp: start,
+            duration: end - start,
+            runLocation: location,
+            success: false,
+            message: ex.Message);
+    }
+
+    telemetryClient.Flush();
+    await Task.Delay(TimeSpan.FromSeconds(frequency));
+}
